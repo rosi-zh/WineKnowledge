b05c7f07a929313c932c63d944e7b4f8a7336653
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-08-11 23:38:55 +0300

    Make active nav

--- wk-app/src/app/core/header/header.component.ts
+++ wk-app/src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 
@@ -7,8 +7,8 @@ import { AuthService } from 'src/app/auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+export class HeaderComponent implements AfterViewInit {
+  constructor(private authService: AuthService, private router: Router, private el: ElementRef, private renderer: Renderer2) {}
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -17,5 +17,35 @@ export class HeaderComponent {
   logout(): void {
     this.authService.logout()
     this.router.navigate(['/']);
+  }
+
+  
+  ngAfterViewInit(): void {
+    this.navbarShrink(); // Shrink the navbar on initial load
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  
+  onWindowScroll() {
+    this.navbarShrink(); // Shrink the navbar on scroll
+  }
+
+  navbarShrink() {
+    const navbarCollapsible = this.el.nativeElement.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      this.renderer.removeClass(navbarCollapsible, 'navbar-shrink');
+    } else {
+      this.renderer.addClass(navbarCollapsible, 'navbar-shrink');
+    }
+  }
+
+  toggleNavbar() {
+    const navbarToggler = this.el.nativeElement.querySelector('.navbar-toggler');
+    if (window.getComputedStyle(navbarToggler).display !== 'none') {
+      navbarToggler.click();
+    }
   }
 }
