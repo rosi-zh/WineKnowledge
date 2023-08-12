4153d32af22553ccc040b523b0d47058b69c6421
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-08-12 13:39:27 +0300

    Email validator update

--- wk-app/src/app/shared/validators/email-validator.ts
+++ wk-app/src/app/shared/validators/email-validator.ts
@@ -2,7 +2,7 @@ import { ValidatorFn } from "@angular/forms";
 
 
 export function emailValidator(): ValidatorFn {
-    const regExp = new RegExp('^[A-Za-z0-9]+[\._A-Za-z0-9-]+@([A-Za-z0-9]+[-\.]?[A-Za-z0-9]+)+(\.[A-Za-z0-9]+[-\.]?[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$');
+    const regExp = /^[A-Za-z0-9]+[\._A-Za-z0-9-]+@([A-Za-z0-9]+[-\.]?[A-Za-z0-9]+)+(\.[A-Za-z0-9]+[-\.]?[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/;
     return (control) => {
         return control.value === '' || regExp.test(control.value)
         ? null
